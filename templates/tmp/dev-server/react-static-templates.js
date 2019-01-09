cc3754beecbf4546430bbe6efa1beda8eb165997
ragu-S <ragu.sivanandha@gmail.com>
2019-01-09 16:16:27 -0500

    Add tmp folder to .gitignore, added additional package.json scripts

--- templates/tmp/dev-server/react-static-templates.js
+++ templates/tmp/dev-server/react-static-templates.js
@@ -1,21 +1,27 @@
 
 
 
-import t_0 from '../../src/pages/404.js'
-import t_1 from '../../src/pages/about.js'
-import t_2 from '../../src/pages/blog.js'
-import t_3 from '../../src/containers/Post'
-import t_4 from '../../src/pages/landing.js'
-import t_5 from '../../src/pages/landing'
+import t_0 from '../../src/pages/404/404'
+import t_1 from '../../src/pages/404/404.js'
+import t_2 from '../../src/pages/About/About.js'
+import t_3 from '../../src/pages/Blog/Blog.js'
+import t_4 from '../../src/pages/Landing/Landing.js'
+import t_5 from '../../src/pages/Landing/Landing'
+import t_6 from '../../src/pages/About/About'
+import t_7 from '../../src/pages/Blog/Blog'
+import t_8 from '../../src/components/Post/Post'
 
 // Template Map
 export default {
-  '../../src/pages/404.js': t_0,
-'../../src/pages/about.js': t_1,
-'../../src/pages/blog.js': t_2,
-'../../src/containers/Post': t_3,
-'../../src/pages/landing.js': t_4,
-'../../src/pages/landing': t_5
+  '../../src/pages/404/404': t_0,
+'../../src/pages/404/404.js': t_1,
+'../../src/pages/About/About.js': t_2,
+'../../src/pages/Blog/Blog.js': t_3,
+'../../src/pages/Landing/Landing.js': t_4,
+'../../src/pages/Landing/Landing': t_5,
+'../../src/pages/About/About': t_6,
+'../../src/pages/Blog/Blog': t_7,
+'../../src/components/Post/Post': t_8
 }
 
-export const notFoundTemplate = "../../src/pages/404.js"
+export const notFoundTemplate = "../../src/pages/404/404"
